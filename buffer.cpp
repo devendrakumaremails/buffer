8885f04edc5b9adbdc74489b3c71574a7f87ca99
Selva <Selvakumar.Ganesan@partner.bmw.de>
2020-02-14 18:23:20 +0100

    coding style changes and removed some unnecessary comments and codes

--- buffer.cpp
+++ buffer.cpp
@@ -7,100 +7,135 @@ Buffer for single thread.
 #include <vector>
 
 template <typename T, unsigned int N>
-class Buffer {
- private:
-  std::vector<T> storage_;
-  // T storage_[N];
-  unsigned int putIndex_{};
-  unsigned int getIndex_{};
-  bool empty_{true};
-  bool full_{false};
+class Buffer
+{
+  private:
+    std::vector<T> storage_;                // other way:  T storage_[N];
+    unsigned int put_index_{};
+    unsigned int get_index_{};
+    bool empty_{true};
+    bool full_{false};
 
- public:
-  Buffer() { storage_.reserve(N); };
-  Buffer(Buffer &) = delete;
-  Buffer(Buffer &&) = delete;
-  Buffer &operator=(Buffer &) = delete;
-  Buffer &operator=(Buffer &&) = delete;
-  explicit operator bool() const { return !empty_; }
-  void operator<<(T &&data) { put(std::forward<T>(data)); }
-
-  bool put(const T &&data)  // universal reference to accept both rvalue and lvalue
-  {
-    bool success = true;
-    if (isFull())  //((getIndex_ == putIndex_) && (!isEmpty()))
+  public:
+    Buffer()
     {
-      std::cout << "Buffer Full!\n";
-      success = false;
+      storage_.reserve(N);
     }
 
-    else {
-      storage_[putIndex_] = data;
-      putIndex_ = (++putIndex_) % N;
-      full_ = getIndex_ == putIndex_;
-      empty_ = false;
-    }
-    print();
-    return success;
-  }
-  T get() {
-    if (isEmpty()) {
-      std::cout << "Buffer Empty!\n";
-      return T();
-    }
+    Buffer(Buffer &) = delete;
+    Buffer(Buffer &&) = delete;
+    Buffer &operator=(Buffer &) = delete;
+    Buffer &operator=(Buffer &&) = delete;
 
-    T &data = storage_[getIndex_];
-    getIndex_ = (++getIndex_) % N;
-    empty_ = getIndex_ == putIndex_;
-    full_ = false;
-
-    print();
-    return data;
-  }
-  bool isEmpty() const { return empty_; }
-  bool isFull() const { return full_; }
-  /*std::outstream& operator << (std::outstream out, const T& data)
-  {
-          
-  }*/
-  void print() const {
-    if (isEmpty()) {
-      std::cout << "Buffer empty!\n";
-      return;
+    explicit operator bool() const
+    {
+      return !empty_;
+    }
+    bool operator << (T &&data)
+    {
+      return Put(std::forward<T>(data)); 
     }
 
-    int itr = getIndex_;
-    std::cout << "Buffer: " << storage_[itr] << '\t';
-    for (itr = (itr + 1) % N; (itr != putIndex_); itr = (itr + 1) % N) std::cout << storage_[itr] << '\t';
-    std::cout << std::endl;
-  }
+    bool Put(const T &&data)  // universal reference to accept both rvalue and lvalue
+    {
+      bool success = true;
+      if (IsFull())
+      {
+        success = false;
+      }
+      else
+      {
+        storage_[put_index_] = data;
+        put_index_ = (++put_index_) % N;
+        full_ = get_index_ == put_index_;
+        empty_ = false;
+      }
+
+      return success;
+    }
+    bool Get(T &output_data_variable)
+    {
+      if (IsEmpty())
+      {
+        return false;
+      }
+      output_data_variable = storage_[get_index_];
+      get_index_ = (++get_index_) % N;
+      empty_ = get_index_ == put_index_;
+      full_ = false;
+
+      return true;
+    }
+    bool IsEmpty() const 
+    {
+      return empty_;
+    }
+    bool IsFull() const
+    {
+      return full_;
+    }
+
+    void Print() const
+    {
+      if (IsEmpty())
+      {
+        std::cout << "Buffer empty!\n";
+        return;
+      }
+
+      int itr = get_index_;
+      std::cout << "Buffer: " << storage_[itr] << '\t';
+      for (itr = (itr + 1) % N; (itr != put_index_); itr = (itr + 1) % N)
+      {
+        std::cout << storage_[itr] << '\t';
+      }
+      std::cout << std::endl;
+    }
 };
 
 int main() {
   Buffer<int, 5> buffer;
   int command = -1, num = 0;
 
-  while (command) {
-    switch (command) {
-      case 1: {
-        buffer.put(num++);  // lvalue
-        buffer << 23;       // rvalue
-        break;
-      }
-      case 2: {
-        if (buffer) {
-          int getVal = buffer.get();
-          std::cout << "getVal: " << getVal << '\n';
+  while (command)
+  {
+    switch (command)
+    {
+      case 1:
+      {
+        if(!buffer.Put(num++))                // lvalue
+        {
+          std::cout << "Buffer is Full!\n";
+        }
+        if(!(buffer << 23))                   // rvalue
+        {
+          std::cout << "Buffer is Full!\n";
         }
         break;
       }
-      default: {
+      case 2:
+      {
+        int getVal;
+        if (buffer.Get(getVal))
+        {
+          std::cout << "getVal: " << getVal << '\n';
+        }
+        else
+        {
+          std::cout << "Buffer is Empty!\n";
+        }
+        
+        break;
+      }
+      default:
+      {
         std::cout << "1: put data into buffer\n"
                   << "2: get data from buffer\n"
                   << "0: exit\n";
         break;
       }
     }
+    buffer.Print();
     std::cout << "\nCommand: ";
     std::cin >> command;
   }
